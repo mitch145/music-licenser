62bf1847cfe4569aa597b8211ea1cb870c96d7f4
Mitch Ball <mitchb145@gmail.com>
2017-03-24 10:37:23 +1100

    First version of profile cards

--- src/routes/profile/artist/index.js
+++ src/routes/profile/artist/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Paper from 'material-ui/Paper';
 
-export default class ArtistProfilePage extends React.Component {
-    render(){
-        return(
-            <div className="page">
-                <h1>Artist Profile</h1>
+export default class AristProfilePage extends React.Component {
+    render() {
+        return (
+            <div className="page artist-profile-page">
+                <Paper className="profile-card">
+                    <div className="left-container">
+                        <img className="profile-picture" src="http://placehold.it/500x500"/>
+                        <div className="text-container">
+                            <p className="artist-name">Artist Name</p>
+                            <p className="artist-bio">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit. Perferendis provident, quasi natus accusantium, ut, at autem ullam totam
+                                fugit nam porro earum, dolorem recusandae! Unde temporibus sunt ratione.
+                                Tenetur, autem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Itaque, beatae, explicabo.</p>
+                        </div>
+                    </div>
+                </Paper>
             </div>
         )
     }
--- src/routes/profile/user/index.js
+++ src/routes/profile/user/index.js
@@ -4,9 +4,19 @@ import Paper from 'material-ui/Paper';
 export default class UserProfilePage extends React.Component {
     render() {
         return (
-            <div className="page">
-                <Paper>
-                    profile
+            <div className="page user-profile-page">
+                <Paper className="profile-card">
+                    <div className="left-container">
+                        <img className="profile-picture" src="http://placehold.it/500x500"/>
+                        <div className="text-container">
+                            <p className="artist-name">Mitch Ball</p>
+                            <p className="artist-bio">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit. Perferendis provident, quasi natus accusantium, ut, at autem ullam totam
+                                fugit nam porro earum, dolorem recusandae! Unde temporibus sunt ratione.
+                                Tenetur, autem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Itaque, beatae, explicabo.</p>
+                        </div>
+                    </div>
                 </Paper>
             </div>
         )
