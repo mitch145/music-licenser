2a37989f1827990ad661451f17177b9c3ea6a30c
Mitch Ball <mitchb145@gmail.com>
2017-03-24 00:12:16 +1100

    First version home-page

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Paper from 'material-ui/Paper';
 
 export default class HomePage extends React.Component {
-    render(){
-        return(
-            <div className="page">
-                <h1>Home</h1>
+    render() {
+        return (
+            <div className="page home-page">
+                <Paper zDepth={1} className="container">
+                    <div className="header">
+                    Header goes here
+                    </div>
+                    <div className="track">
+                        Card thngo goes here
+                    </div>
+                    <div className="track">
+                        Card thngo goes here
+                    </div>
+                    <div className="track">
+                        Card thngo goes here
+                    </div>
+                </Paper>
             </div>
         )
     }
